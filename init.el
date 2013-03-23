743c4c3211b47f3b22cdd7ee93b6aec658bead26
Philip Ingram <philpjingram@yahoo.com>
2013-03-23 09:30:20 -0400

    improved ReadMe and updated init.el for 24.3

--- init.el
+++ init.el
@@ -1,37 +1,23 @@
 ;; General Config
 ;; ---------------
 (setq backup-directory-alist (quote ((".*" . "~/.emacs.d/"))))
-;; disable backup files
-(setq make-backup-files nil)
+(setq make-backup-files nil) ;; disable backup files
 (setq auto-save-default nil) ; turns off that blasted auto-save shit
 (add-to-list 'default-frame-alist '(height . 50))
 (add-to-list 'default-frame-alist '(width . 170))
-(global-linum-mode 1)
-(delete-selection-mode 1)
-(blink-cursor-mode t)
-(electric-pair-mode t)
+(global-linum-mode t) ;; line numbers on the side.
+(delete-selection-mode t) ;; highlight a word and start typing, and it will delete the word and put your typed characters in it's place. highly annoying if not there.
+(blink-cursor-mode t) ;; blinking cursor easier to find, trust.
+(electric-pair-mode t) ;; allows for matching "" and () and []
 (tool-bar-mode -1); turn off toolbar
-(hl-line-mode 1) ; turn on highlight line mode
-(setq inhibit-startup-message t)
-(global-set-key "\C-x\C-k" 'kill-this-buffer)
-(add-hook 'ruby-mode-hook
-      (lambda () (local-set-key (kbd "RET") 'reindent-then-newline-and-indent)))
-(fset 'yes-or-no-p 'y-or-n-p)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(hl-line-mode t) ; turn on highlight line mode
+(setq inhibit-startup-message t) ;; meh, not needed on your 300+ day of using
+(global-set-key "\C-x\C-k" 'kill-this-buffer) ;; Kill the buffer you no longer want in your frame.
+(fset 'yes-or-no-p 'y-or-n-p) ;; uses y instead of yes. less typing
+(add-hook 'before-save-hook 'delete-trailing-whitespace) ;; deletes all whitespace that isn't needed.
 (set-face-attribute 'default nil :height 160)
-;; Move windows by using M+arrowkeyleft/right/whatever
-(windmove-default-keybindings 'meta)
-;; making ruby mode take effect in our odd Rails project files
-(setq auto-mode-alist (cons '("\\.rake\\'" . ruby-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("Gemfile*" . ruby-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("Capfile" . ruby-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.erb\\'" . rhtml-mode) auto-mode-alist))
-;;; <M-x> to comment-or-uncomment-region (TextMate compatibility)
-;; (global-set-key (kbd "M-/") 'comment-or-uncomment-region)
-(global-set-key (kbd "\"") 'insert-pair)
-(global-set-key (kbd "C-S-m") 'magit-status)
-
-
+(windmove-default-keybindings 'meta) ;; Move windows by using M+arrowkeyleft/right/whatever
+(global-set-key (kbd "C-S-m") 'magit-status) ;; if you use git, great way to see wtf is going on
 
 ;; el-get config
 ;; -------------
@@ -39,11 +25,9 @@
 (require 'el-get)
 (setq el-get-packages
        '(
-	 autopair
 	 haml-mode
 	 magit
 	 rinari
-	 ruby-mode
 	 rvm
 	 ruby-end
 	 ruby-compilation
@@ -55,40 +39,55 @@
 
 (el-get 'sync el-get-packages)
 
-;; el-get Individual Recipe configs
-;; --------------------------------
-;; Color-Theme
-;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+;; Built In Theme
+;; adwaita 	deeper-blue 	dichromacy 	leuven 	light-blue 	manoj-dark
+;; misterioso 	tango 	tango-dark 	tsdh-dark 	tsdh-light 	wheatgrass
+;; whiteboard 	wombat
+;; --------------
  (load-theme 'wombat)
-;; (load-theme 'zenburn t)
 ;; ido mode for file finding
+;; -------------------------
 (require 'ido)
 (ido-mode 1)
-(setq ido-enable-prefix -1
-ido-enable-flex-matching 1
+(setq ido-enable-prefix nil
+ido-enable-flex-matching t
 ido-use-filename-at-point 'guess
 ido-max-prospects 10)
 ;;; additional keys (copied from source file)
 (defun ido-my-keys ()
   "Add my keybindings for ido."
   (define-key ido-completion-map " " 'ido-next-match))
-
+;; Ruby Mode Adjustments
+;; --------------------
+;; making ruby mode take effect in our odd Rails project files
+(setq auto-mode-alist (cons '("\\.rake\\'" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Gemfile*" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Capfile" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.erb\\'" . rhtml-mode) auto-mode-alist))
+(add-hook 'ruby-mode-hook
+     (lambda () (local-set-key (kbd "RET") 'reindent-then-newline-and-indent))) ;; hitting enter will indent.
+(global-set-key (kbd "\C-c\C-c") 'comment-or-uncomment-region) ;; highlight region and comment
 ;;Rhtml mode for erb files
+;; --------------------
 (require 'rhtml-mode)
 ;; Haml mode stuff and Haml mode was not loading hope it does now.
+;; --------------------
 (require 'haml-mode)
 (add-hook 'haml-mode-hook
                   '(lambda ()
                          (setq indent-tabs-mode nil)
                          (define-key haml-mode-map "\C-m" 'newline-and-indent)))
 ;; Yasnippet config
+;; --------------------
 ;; Develop and keep personal snippets under ~/emacs.d/mysnippets
-(yas/global-mode 1)
+(yas/global-mode t)
 (setq yas/root-directory '("~/.emacs.d/snippets"
 			 "~/.emacs.d/el-get/yasnippet/snippets"))
 ;; Load the snippets
 (mapc 'yas/load-directory yas/root-directory)
 (custom-set-variables
+;; No clue but leaving it config
+;; --------------------
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
